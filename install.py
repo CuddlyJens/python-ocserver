32f6ba03bfd5da7d21fa7db00d21bd8e119879d6
CuddlyJens <jens.bartsch.97@gmail.com>
2023-04-15 01:49:58 +0200

    aaa

--- install.py
+++ install.py
@@ -7,7 +7,7 @@ print('done')
 # Installation ownCloud
 os.chdir('/var/www/')
 print("Current working directory: {0}".format(os.getcwd()))
-time.sleep(5)
+time.sleep(2)
 os.system('wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \
 tar -xjf owncloud-complete-latest.tar.bz2 && \
 chown -R www-data. owncloud')
