93565c36db42cec9f5da01285eb2f11351dab42a
CuddlyJens <jens.bartsch.97@gmail.com>
2023-04-15 00:16:56 +0200

    aaa

--- install.py
+++ install.py
@@ -1,12 +1,10 @@
 import os
-import time
 # Enable the Recommendet Apache Modules
 os.system('a2enmod dir env headers mime rewrite setenvif')
 os.system('systemctl restart apache2')
 print('done')
 # Installation ownCloud
-os.system('cd /var/www/')
-time.sleep(5)
-os.system('wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \
+os.popen('cd /var/www/')
+os.popen('wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \
 tar -xjf owncloud-complete-latest.tar.bz2 && \
 chown -R www-data. owncloud')
