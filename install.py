b69d46eee38c1b07aed0a4b4f629963efd5baecd
CuddlyJens <jens.bartsch.97@gmail.com>
2023-04-15 00:25:27 +0200

    aaa

--- install.py
+++ install.py
@@ -5,8 +5,9 @@ os.system('a2enmod dir env headers mime rewrite setenvif')
 os.system('systemctl restart apache2')
 print('done')
 # Installation ownCloud
-os.popen('cd /var/www/').read
+os.chdir('/var/www/')
+print("Current working directory: {0}".format(os.getcwd()))
 time.sleep(5)
-os.popen('wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \
+os.system('wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \
 tar -xjf owncloud-complete-latest.tar.bz2 && \
 chown -R www-data. owncloud')
