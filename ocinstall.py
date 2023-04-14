baf0b7cf2b196f0f3f73c87fe2f93aeae4da17a8
CuddlyJens <jens.bartsch.97@gmail.com>
2023-04-14 19:07:13 +0200

    aaa

--- ocinstall.py
+++ ocinstall.py
@@ -67,16 +67,8 @@ os.system('mysql_secure_installation')
 # Enable the Recommendet Apache Modules
 os.system('a2enmod dir env headers mime rewrite setenvif')
 os.system('systemctl restart apache2')
-
-# # Installation ownCloud
-# os.system("cd /var/www/")
-# os.system("wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \ tar -xjf owncloud-complete-latest.tar.bz2 && \ chown -R www-data. owncloud")
-
-# os.system('occ maintenance:install \
-#     --database "mysql" \
-#     --database-name "owncloud" \
-#     --database-user "root" \
-#     --database-pass "testcloud" \
-#     --data-dir "/var/www/owncloud/data" \
-#     --admin-user "admin" \
-#     --admin-pass "admin')
+print('done')
+# Installation ownCloud
+os.system("cd /var/www/")
+os.system("wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \ tar -xjf owncloud-complete-latest.tar.bz2 && \ chown -R www-data. owncloud")
+print('Please Configure now the Database')
