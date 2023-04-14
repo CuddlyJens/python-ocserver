8a0598c6e1a8cb33fef28902d9588061aec904bf
CuddlyJens <jens.bartsch.97@gmail.com>
2023-04-14 18:43:58 +0200

    aaa

--- ocinstall.py
+++ ocinstall.py
@@ -68,15 +68,15 @@ os.system('mysql_secure_installation')
 os.system('a2enmod dir env headers mime rewrite setenvif')
 os.system('systemctl restart apache2')
 
-# Installation ownCloud
-os.system("cd /var/www/")
-os.system("wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \ tar -xjf owncloud-complete-latest.tar.bz2 && \ chown -R www-data. owncloud")
+# # Installation ownCloud
+# os.system("cd /var/www/")
+# os.system("wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \ tar -xjf owncloud-complete-latest.tar.bz2 && \ chown -R www-data. owncloud")
 
-os.system('occ maintenance:install \
-    --database "mysql" \
-    --database-name "owncloud" \
-    --database-user "root" \
-    --database-pass "testcloud" \
-    --data-dir "/var/www/owncloud/data" \
-    --admin-user "admin" \
-    --admin-pass "admin')
+# os.system('occ maintenance:install \
+#     --database "mysql" \
+#     --database-name "owncloud" \
+#     --database-user "root" \
+#     --database-pass "testcloud" \
+#     --data-dir "/var/www/owncloud/data" \
+#     --admin-user "admin" \
+#     --admin-pass "admin')
