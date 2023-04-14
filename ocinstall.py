c373a998f2f835a39a493d42837a8010e8a42c7f
CuddlyJens <jens.bartsch.97@gmail.com>
2023-04-14 14:41:47 +0200

    update

--- ocinstall.py
+++ ocinstall.py
@@ -1,7 +1,9 @@
 import os
 
+# update system
 os.system('apt update && apt upgrade -y')
 
+# install Required and Recommended Packages, Configure Apache
 os.system('sudo add-apt-repository ppa:ondrej/php -y')
 os.system('sudo apt update && sudo apt upgrade -y')
 os.system('apt install -y \
@@ -17,3 +19,27 @@ os.system('apt-get install -y php7.4-smbclient')
 os.system('echo "extension=smbclient.so" > /etc/php/7.4/mods-available/smbclient.ini')
 os.system('phpenmod smbclient')
 os.system('systemctl restart apache2')
+os.system('apt install -y \
+  unzip bzip2 rsync curl jq \
+  inetutils-ping  ldap-utils\
+  smbclient')
+
+VHC = open('/etc/apache2/sites-available/owncloud.conf', 'w')
+VHC.write('<VirtualHost *:80> \
+# uncommment the line below if variable was set \
+#ServerName \$my_domain \
+DirectoryIndex index.php index.html \
+DocumentRoot /var/www/owncloud \
+<Directory /var/www/owncloud> \
+  Options +FollowSymlinks -Indexes \
+  AllowOverride All \
+  Require all granted \
+\
+ <IfModule mod_dav.c> \
+  Dav off \
+ </IfModule> \
+\
+ SetEnv HOME /var/www/owncloud \
+ SetEnv HTTP_HOME /var/www/owncloud \
+</Directory> \
+</VirtualHost>')
